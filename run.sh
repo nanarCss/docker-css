8b83838487b3ba9845f7108f615a88903b971909
nanarCss <71874174+nanarCss@users.noreply.github.com>
2020-10-05 16:07:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,8 @@ mysql_container_name=mysqlDB
 PORT=8080
 
 # MySQL
-docker run --name $mysql_container_name -e MYSQL_ROOT_PASSWORD=$rt_pass -e $# Apache / WordPress / PhpMyAdmin
-docker run -d -p $PORT:80 -v /web:/var/www/localhost/htdocs pennarguear/$Do$
+docker run --name $mysql_container_name -e MYSQL_ROOT_PASSWORD=$rt_pass -e MYSQL_DATABASE=$db -d mysql:5.7.31
+# Apache / WordPress / PhpMyAdmin
+docker run -d -p $PORT:80 -v /web:/var/www/localhost/htdocs pennarguear/$DockIm
 
 # docker-compose up -d
