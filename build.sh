210deec36deb947b22b6f87ce820c321af4486f6
nanarCss <71874174+nanarCss@users.noreply.github.com>
2020-10-05 13:39:09 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 # This script will build the image of dockerfile choosen
-dockerfile=apache2-eseo
+#dockerfile=apache2-eseo
 
-#docker build -t eseo/$dockerfile - < $dockerfile
-docker build -t pennarguear/$dockerfile - < $dockerfile
+#docker build -t pennarguear/$dockerfille - < $dockerfile
+docker-compose build
